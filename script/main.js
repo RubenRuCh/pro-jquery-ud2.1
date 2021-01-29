5d9e76e68734d7572373a2bd2510b0801a3aee1d
Rubén Rüger <ruben_27895@hotmail.com>
2021-01-29 20:05:43 +0100

    Practica 1. UD2

---
+++ script/main.js
@@ -0,0 +1,32 @@
+$(() => {
+  // Insert a new css class
+  $(
+    "<style type='text/css'> .highlight{ background-color: yellow; color: black; font-weight: bold;} </style>"
+  ).appendTo('head');
+
+  // Get p as a cache variable
+  $p = $('p');
+
+  // Get all words of p
+  $words = $p.text().trim().split(' ');
+
+  // Clean <p>
+  $p.text(' ');
+
+  // Insert all words again but surrounded by some cool spans
+  for (const word of $words) {
+    if (word.length > 0) {
+      $p.append(`<span>${word.trim()}</span> `);
+    }
+  }
+
+  // Add eventlistener on click to every word (selected by span)
+  $p.children('span').each((index, word) => {
+    $(word).on('click', function (event) {
+      event.preventDefault();
+
+      //$(this).toggleClass('highlight'); // Change status of the class. Is not required but it would be cooler...
+      $(this).addClass('highlight'); // Just add the class :(
+    });
+  });
+});
